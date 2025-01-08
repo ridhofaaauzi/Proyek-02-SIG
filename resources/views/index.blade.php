5ea6ef4cd243e5c07fe0e02655b593f6ba935124
Bobogeng <irsalfathifarhat@gmail.com>
2025-01-09 00:56:46 +0700

    Fix Card Height

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -388,41 +388,43 @@
         </div>
 
         <!-- Details Section -->
-        <div class="col-span-12 lg:col-span-4 bg-white p-6 rounded shadow">
-            <div class="w-fit px-4 py-1 bg-green-100 rounded">
-                <h2 class="font-medium text-green-600">Terkonfirmasi</h2>
-            </div>
-            <p class="mt-2 text-gray-600 text-sm">{{ $birth_rate->created_at->format('d M Y, H:i:s') }}</p>
-            <h3 class="mt-4 text-xl font-bold">{{ $birth_rate->district->name }}</h3>
-            <div class="mt-4">
-                <div class="bg-slate-100 flex flex-col py-2 rounded-md">
-                    <div class="flex justify-between space-x-2 px-4 p-2">
-                        <div class="flex space-x-2 items-center">
-                            <x-lucide-users class="w-5 h-5 text-red-500" />
-                            <p class="font-medium text-start text-slate-500">Populasi:</p>
+        <div class="col-span-12 lg:col-span-4">
+            <div class="bg-white p-6 rounded shadow">
+                <div class="w-fit px-4 py-1 bg-green-100 rounded">
+                    <h2 class="font-medium text-green-600">Terkonfirmasi</h2>
+                </div>
+                <p class="mt-2 text-gray-600 text-sm">{{ $birth_rate->created_at->format('d M Y, H:i:s') }}</p>
+                <h3 class="mt-4 text-xl font-bold">{{ $birth_rate->district->name }}</h3>
+                <div class="mt-4">
+                    <div class="bg-slate-100 flex flex-col py-2 rounded-md">
+                        <div class="flex justify-between space-x-2 px-4 p-2">
+                            <div class="flex space-x-2 items-center">
+                                <x-lucide-users class="w-5 h-5 text-red-500" />
+                                <p class="font-medium text-start text-slate-500">Populasi:</p>
+                            </div>
+                            <p class="font-bold text-end text-slate-700">
+                                {{ $district_data->population }}</p>
                         </div>
-                        <p class="font-bold text-end text-slate-700">
-                            {{ $district_data->population }}</p>
-                    </div>
-                    <div class="flex justify-between space-x-2 px-4 p-2">
-                        <div class="flex space-x-2 items-center">
-                            <x-lucide-target class="w-5 h-5 text-green-500" />
-                            <p class="font-medium text-start text-slate-500">Area:</p>
+                        <div class="flex justify-between space-x-2 px-4 p-2">
+                            <div class="flex space-x-2 items-center">
+                                <x-lucide-target class="w-5 h-5 text-green-500" />
+                                <p class="font-medium text-start text-slate-500">Area:</p>
+                            </div>
+                            <p class="font-bold text-end text-slate-700">
+                                {{ $district_data->area }} Km²</p>
                         </div>
-                        <p class="font-bold text-end text-slate-700">
-                            {{ $district_data->area }} Km²</p>
-                    </div>
-                    <div class="flex justify-between space-x-2 px-4 p-2">
-                        <div class="flex space-x-2 items-center">
-                            <x-lucide-baby class="w-5 h-5 text-blue-500" />
-                            <p class="font-medium text-start text-slate-500">Total Kelahiran:</p>
+                        <div class="flex justify-between space-x-2 px-4 p-2">
+                            <div class="flex space-x-2 items-center">
+                                <x-lucide-baby class="w-5 h-5 text-blue-500" />
+                                <p class="font-medium text-start text-slate-500">Total Kelahiran:</p>
+                            </div>
+                            <p class="font-bold text-end text-slate-700">{{ $birth_rate->total }}</p>
                         </div>
-                        <p class="font-bold text-end text-slate-700">{{ $birth_rate->total }}</p>
                     </div>
                 </div>
+                <button id="openModal" class="mt-6 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Lihat
+                    Detail</button>
             </div>
-            <button id="openModal" class="mt-6 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Lihat
-                Detail</button>
         </div>
 
         <div id="loading">
