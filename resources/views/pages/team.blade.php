239f13f12edbf62dac6f770aef4b932743309a03
Bobogeng <irsalfathifarhat@gmail.com>
2025-01-10 21:21:01 +0700

    Fix Spacing & Responsiveness

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -8,10 +8,10 @@
 
 @section('content')
     <main class="relative max-w-7xl mx-auto lg:my-6 lg:px-8">
-        <div class="text-center py-8 sm:mt-10 sm:mb-16 px-4 sm:p-0">
+        <div class="text-center pt-8 sm:mt-10 sm:mb-16 px-4 sm:p-0">
             <h1 class="text-2xl sm:text-3xl font-bold sm:leading-9 text-gray-800">Peta Kelahiran Kota Depok: Data, Tren, dan
                 Masa Depan</h1>
-            <p class="text-base font-normal text-gray-600 mt-2">
+            <p class="text-base font-normal text-gray-600 sm:mt-2 mt-6">
                 Menyediakan data kelahiran yang akurat dan interaktif untuk membantu memahami pertumbuhan populasi dan
                 perencanaan wilayah yang lebih baik.
             </p>
@@ -22,7 +22,7 @@
                 class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-[320px] sm:max-w-none mx-auto">
                 @foreach ($team_members as $member)
                     <div
-                        class="group max-[320px]:rounded-none min-[320px]:rounded sm:shadow-md overflow-hidden sm:hover:shadow-lg duration-100 ease-linear">
+                        class="group max-[320px]:rounded-none min-[321px]:rounded sm:shadow-md overflow-hidden sm:hover:shadow-lg duration-100 ease-linear">
                         <div class="relative w-full h-72 overflow-hidden">
                             <div class="absolute top-0 left-0 w-full h-full bg-gray-300 animate-pulse skeleton"></div>
                             <img src="{{ $member['photo'] }}" alt="{{ $member['name'] }}"
